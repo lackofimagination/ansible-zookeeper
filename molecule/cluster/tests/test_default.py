b66bcbc4d604aeb381b740636cd1235eff9269a8
Huang Xuxing <grubbybloodorc@gmail.com>
2018-11-02 11:22:02 +0800

    Add cluster test

--- molecule/cluster/tests/test_default.py
+++ molecule/cluster/tests/test_default.py
@@ -6,9 +6,32 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+def test_zookeeper_user(host):
+    u = host.user('zookeeper')
+
+    assert u.name == 'zookeeper'
+    assert 'zookeeper' in u.groups
+
+
+def test_zoo_cfg_file(host):
+    f = host.file('/etc/zookeeper/zoo.cfg')
 
     assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+
+
+def test_zookeeper_init_file(host):
+    f = host.file('/etc/init.d/zookeeper-server')
+
+    assert f.exists
+
+
+def test_zookeeper_pid_file(host):
+    f = host.file('/var/run/zookeeper/zookeeper-server.pid')
+
+    assert f.exists
+
+
+def test_myid_file(host):
+    f = host.file('/var/lib/zookeeper/myid')
+
+    assert f.exists
